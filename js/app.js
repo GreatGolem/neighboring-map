f6908bba4562ca3523d30d5c488fddfde12b4e00
GreatGolem-laptop <siyu.wu.91@gmail.com>
2016-10-26 17:58:21 -0400

    minor fix

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //This is the model.
-$.getJSON("test.json", function(json) {
+$.getJSON("locations.json", function(json) {
   console.log(json); // this will show the info it in firebug console
 });
 
