4d67b13b4e6138e5e7620c0af91deaec30462baa
GreatGolem-laptop <siyu.wu.91@gmail.com>
2016-10-27 20:48:47 -0400

    Map and Sidebar positioned

--- js/app.js
+++ js/app.js
@@ -1,20 +1,55 @@
 //An ajax call to update list of location with the json data file.
-//It's written at the beginning but actually executed after knockout binding.
-$.getJSON("locations.json", function(json) {
-  console.log(json);
-  //Add locations to the list and add marker to the map.
-  json.forEach(function(locItem){
-    VW.locList.push(locItem);
-    var marker = new google.maps.Marker({
-      position: locItem.latlng,
-      map: initMap()
-    });
-  });
-});
+//It failed badly so I give it up and just put data in js file directly.
+
+// $.getJSON("locations.json", function(json) {
+//   console.log(json);
+//   //Add locations to the list and add marker to the map.
+//   json.forEach(function(locItem){
+//     VW.locList.push(locItem);
+//     var marker = new google.maps.Marker({
+//       position: locItem.latlng,
+//       map: initMap()
+//     });
+//   });
+// });
+
+
+//Here's the model.
+var data = [
+    {
+        name : 'Home',
+        latlng : {lat: 40.454828, lng: -86.920855},
+        description : 'Home. Sweet home.'
+    }, {
+        name : 'Lab',
+        latlng : {lat: 40.426642, lng: -86.913394},
+        description : 'The place I work.'
+    }, {
+        name : 'Payless',
+        latlng : {lat: 40.455023, lng: -86.917591},
+        description : 'The closest supermarket to my home.'
+    }, {
+        name : 'Walmart',
+        latlng : {lat: 40.457091, lng: -86.933015},
+        description : 'A larger supermarket close to my home.'
+    }, {
+        name : 'Purdue Memorial Union',
+        latlng : {lat: 40.424700, lng: -86.911325},
+        description : 'Fulled with restaurants and other random facilities.'
+    }, {
+        name : 'Yichiban',
+        latlng : {lat: 40.417147, lng: -86.893190},
+        description : 'Best Chinese restaurant in town.'
+    }, {
+        name : 'C&T Market',
+        latlng : {lat: 40.423287, lng: -86.899522},
+        description : 'A small Chinese supermarket with nice meat and snack supply.'
+    }
+]
 
 //Here's the viewmodel.
 var ViewModel = function() {
-  this.locList = ko.observableArray([]);
+  this.locList = ko.observableArray(data);
 };
 ko.applyBindings(VW = new ViewModel());
 
@@ -25,5 +60,10 @@ function initMap() {
     zoom: 13,
     center: initial
   });
-  return map;
+  data.forEach(function(item) {
+    var marker = new google.maps.Marker({
+      position: item.latlng,
+      map: map
+    });
+  })
 }
